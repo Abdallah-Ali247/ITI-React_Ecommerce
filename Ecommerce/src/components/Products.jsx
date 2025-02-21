d789ffb7b6d1e142b83cd031f5e01424f5b3ed39
Abdallah-Ali247 <aa2447454@gmail.com>
2025-02-21 16:17:51 +0200

    finaaaaaaal

--- Ecommerce/src/components/Products.jsx
+++ Ecommerce/src/components/Products.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../redux/slices/productSlice";
@@ -25,6 +24,7 @@ const Products = () => {
       return;
     }
     dispatch(addToCart({ product, userId: user.id }));
+    alert("Add Product to  card ?")
   };
 
 
@@ -87,9 +87,12 @@ const Products = () => {
                 <div className="card-body text-center">
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
-                  <button className="btn btn-primary w-100">Add to Cart</button>
+                  <button className="btn btn-primary w-100"onClick={() => handleAddToCart(product)}>
+                    Add to Cart</button>
                 </div>
               </div>
+
+              
             </Col>
           ))}
         </Row>
